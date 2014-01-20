004b8ae897e39a38b2e55e997b318c06f9fc8b41
Jeronimo Nuñez <jeronimonunez@gmail.com>
2014-01-20 01:58:39 -0300

    First commit
    Commit all files!!!

---
+++ js/main.js
@@ -0,0 +1,34 @@
+
+function updateScrollbar() {
+    var width = 372;
+    var height = 312;
+
+    $("#scroller").width(width).height(height);
+
+    // update perfect scrollbar
+    $("#scroller").perfectScrollbar('update');
+}	
+
+
+$(function() {
+
+	//first of all
+	$('#scroller .text').html( $('#home').html() );
+	$('#home-tab a').addClass('active');
+
+    $("#scroller").perfectScrollbar({
+    	suppressScrollX: true
+    });
+
+    $('.tabs ul li a').on('click', function() {
+    	var target = $(this).data('content'),
+    		html = $( target ).html();
+
+		$('#scroller .text').html( html );
+
+		$("#scroller").perfectScrollbar('update');
+
+		$(this).parents('ul').find('a').removeClass('active');
+		$(this).addClass('active');
+    })
+});
